07bcb765e623919cb0205b27153286ee6bd5378c
Andrej-Novik <andrejnovikweb@gmail.com>
2021-12-04 20:16:59 +0300

    T-2: - finished catalog

---
+++ src/useCases/actions/objects.js
@@ -0,0 +1,26 @@
+import {
+  SET_STATE_OBJECTS,
+  CHANGE_OBJECT_LOADER,
+  ERROR_OBJECT_USERS,
+} from "../actionTypes/objects";
+import Repository from "../../repository";
+
+export const setStateObjects = (objects) => {
+  return { type: SET_STATE_OBJECTS, payload: objects };
+};
+export function objectsLoading(value) {
+  return { type: CHANGE_OBJECT_LOADER, value };
+}
+export function loadingError(value) {
+  return { type: ERROR_OBJECT_USERS, value };
+}
+
+export const getObjectsFromBD = () => async (dispatch) => {
+  dispatch(objectsLoading(true));
+  const { value, error } = await Repository.APIObjects.getObjects();
+  if (!value || error) {
+    dispatch(loadingError(true));
+  }
+  dispatch(setStateObjects(value));
+  dispatch(objectsLoading(false));
+};
